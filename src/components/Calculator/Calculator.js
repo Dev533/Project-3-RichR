a3ed0a021a208eae611c083e12689bc111d98c19
Dev533 <jb.developer.fs@gmail.com>
2018-06-23 11:43:28 -0700

    created new branch called add-state to handle adding state to Calculator.js

---
+++ src/components/Calculator/Calculator.js
@@ -0,0 +1,84 @@
+import React, { Component } from "react";
+// import Math from "../Math/Math";
+
+
+class Calculator extends Component {
+  state = {
+    netIncome: "",
+    payFrequency: "",
+    monthlyHousing: "",
+    monthlyInsurance: "",
+    monthlyUtilities: ""
+  };
+  
+ handleInputChange = event => {
+   // Getting the value and name of the input which triggered the change.
+   let value = event.target.value;
+   const name = event.target.name;
+
+    // Updating the input's state
+    this.setState({
+      [name]: value
+    });
+ };
+
+  render() {
+    return(
+      <div>
+      <h1> Fill in the info below. </h1>
+      <h2> Income </h2>
+      <form>
+        <label>
+        Net income on latest paycheck
+          <input type="text" name="name" value={this.state.netIncome} onChange={this.handleInputChange}/>
+        </label>
+        <br />
+        <br />
+        <label>
+        Pay Frequency
+          <input type="radio" />
+          Weekly
+        </label>
+        <label>
+          <input type="radio" />
+          Biweekly
+        </label>
+        <label>
+          <input type="radio" />
+          Semi-Monthly
+        </label>
+        <label>
+          <input type="radio" />
+          Monthly
+        </label>
+        <h2> Expenses </h2>
+        <p>Enter how much you pay each month for each of the following bills.</p>
+        <label>
+        Mortgage or Rent
+          <input type="text" name="name" />
+        </label>
+        <br />
+        <br />
+        <label>
+        Insurance Payments
+          <input type="text" name="name" />
+        </label>
+        <br />
+        <br />
+        <label>
+        Phone and Utilities
+          <input type="text" name="name" />
+        </label>
+        <br />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+     
+    </div>
+    )
+  }
+};
+ 
+
+
+export default Calculator;
