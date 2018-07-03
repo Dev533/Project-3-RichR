0b394d5f9b9a020a4c336dbeeb5cdd6a408ba159
Dev533 <jb.developer.fs@gmail.com>
2018-07-02 19:33:45 -0700

    I made some changes?

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://richer:richer1@ds125048.mlab.com:25048/heroku_dhqqvn7d",
-  secretOrKey: "secret"
+  mongoURI: process.env.mongoURI,
+  secretOrKey: process.env.secret
 };
